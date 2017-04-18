0fd2d8ee36454c80899ac535414d8962f283a2c4
ytgoo95 <ytgoo95@DESKTOP-8U4IA5I>
2017-04-18 12:30:44 +0900

    실습과제8 제출

---
+++ JavaPractice/src/org/dimigo/oop/PiggyBankTest.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *   |_ PiggyBankTest
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 4. 18.
+ * </pre>
+ *
+ * @author	    : 구윤태
+ * @vresion     : 1.0
+ */
+public class PiggyBankTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		FamilyMember[] mems = {
+				new FamilyMember("아빠"),
+				new FamilyMember("엄마"),
+				new FamilyMember("나"),
+				new FamilyMember("남동생")
+		};
+		FamilyMember.printMemberCnt();
+		PiggyBank.putMoney(mems[0],10000);
+		PiggyBank.putMoney(mems[1],5000);
+		PiggyBank.putMoney(mems[2],2000);
+		PiggyBank.putMoney(mems[3],1000);
+		PiggyBank.printBalance();
+		PiggyBank.putMoney(mems[2],1000);
+		PiggyBank.printBalance();
+		
+	}
+
+}
